3b11edf2992072abece80000e164fc121028b171
Aldren Letada <60842892+GarudaKurt@users.noreply.github.com>
2025-02-10 17:46:54 +0800

    Update master.ino

--- master/master.ino
+++ master/master.ino
@@ -3,6 +3,9 @@
 
 SoftwareSerial BTSerial(2, 3); // HC-05 TX to D2, RX to D3
 
+unsigned long prevTime = 0;
+const unsigned long interval = 5000;
+
 void setup() {
   Serial.begin(9600);  
   BTSerial.begin(9600);
@@ -21,16 +24,21 @@ void loop() {
       Serial.println("ALERT: Alarm Button Pressed on Slave!");
       delay(5000);  
       buzzerStop();
-    }
-
-    if (received == "active") {
+    } else if (received == "active") {
+      prevTime = millis();
       buzzerStop();
       Serial.println("Activated!");
     } else {
+      Serial.println("Data from slave: "+ received);
       Serial.println("De-activated!");
       buzzerStart();
     }
-
-    BTSerial.println("Onstate"); // Send state update back to slave
+    //BTSerial.println("Onstate"); // Send state update back to slave
   }
+
+  if (millis() - prevTime > interval) {
+    Serial.println("Slave Disconnected! Activating buzzer!");
+    buzzerStart(); // Trigger buzzer when slave is out of range
+  }
+
 }
